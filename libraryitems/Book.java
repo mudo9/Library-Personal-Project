420eec1e4b2cb8028f9de60804ea9827e43dd89a
Michael Udo <mudo1@sheffield.ac.uk>
2024-09-10 23:02:32 +0100

    The book class

---
+++ libraryitems/Book.java
@@ -0,0 +1,69 @@
+package libraryitems;
+/**
+ * A java class that stores information about a book.
+ * @author Michael Udo
+ */
+
+
+public class Book {
+    private String bookId;
+    private String title;
+    private String author;
+    private boolean isAvailable;
+
+    public Book(String bookId, String title, String author, boolean isAvailable) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.isAvailable = isAvailable;
+    }
+
+    
+
+    public String getbookId() {
+        return bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean getIsAvailable() {
+        return isAvailable;
+    }
+
+    public void setBookId(String bookId) {
+        this.bookId = bookId;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setIsAvailable(boolean isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+
+    public String toString() {
+        if (isAvailable == true ) {
+            return title + ", ID: " + bookId + ", written by " + author + " is available.";
+        } else  {
+            return title + ", ID: " + bookId + ", written by " + author + " is not available.";
+        } 
+        
+    }
+
+    public String borrowBook() {
+        return this.title + " has been borrowed.";
+    }
+
+    
+}
